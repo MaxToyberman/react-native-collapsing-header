5d3f2e8ac93a4835591315af331bb1be1ad0aa08
Max Toyberman <maxtoyberman@gmail.com>
2018-02-20 11:56:07 +0200

    create collapsing header

---
+++ index.js
@@ -0,0 +1,88 @@
+
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import {Animated, ScrollView, StyleSheet, View, Dimensions, Text, PanResponder} from 'react-native'
+
+class CollapsingHeader extends Component {
+
+    constructor(props) {
+        super(props);
+        // scrolling distance
+        this.HEADER_SCROLL_DISTANCE = props.headerMaxHeight - props.headerMinHeight
+        this.state = {
+            scrollY: new Animated.Value(0)
+        }
+    }
+
+    render() {
+
+        const headerPosition = this.state.scrollY.interpolate({
+            inputRange: [0, this.HEADER_SCROLL_DISTANCE],
+            outputRange: [0, - this.HEADER_SCROLL_DISTANCE],
+            extrapolate: 'clamp' // clamp so translateY can’t go beyond -160
+        })
+
+        return (
+            <View style={styles.container}>
+                <Animated.View style={[styles.header,
+                    {transform: [{
+                        translateY: headerPosition
+                    }]
+                }]} >
+                    {this.props.header}
+                </Animated.View>
+
+                <Animated.ScrollView
+                    contentContainerStyle={styles.contentContainer} 
+                    scrollEventThrottle={this.props.scrollEventThrottle}
+                    bounces={false}
+                    style={{zIndex: -1}}
+                    onScroll={Animated.event(
+                        [{ nativeEvent: { contentOffset: { y: this.state.scrollY } } }],
+                        {
+                          useNativeDriver: true
+                        },
+                      )}
+                >
+                    {this.props.children}
+                </Animated.ScrollView>
+            </View>)
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      flexDirection: 'column',
+      alignItems: 'center',
+    },
+    header: {
+        position: 'absolute',
+        backgroundColor: 'white',
+        top: 0,
+        left: 0,
+        right: 0,
+        height: this.props.headerMaxHeight,
+        width,
+    },
+    contentContainer: {
+        width,
+        marginTop: HEADER_MAX_HEIGHT
+    },
+});
+
+
+CollapsingHeader.propTypes = {
+    header: PropTypes.element,
+    headerMaxHeight: PropTypes.number,
+    headerMinHeight: PropTypes.number,
+    scrollEventThrottle: PropTypes.number
+}
+
+CollapsingHeader.defaultProps = {
+    headerMaxHeight: 199,
+    headerMinHeight: 117,
+    scrollEventThrottle: 16
+}
+
+export default CollapsingHeader;
